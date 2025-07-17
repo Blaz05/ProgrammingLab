a7d38a1a60929296f10c1ab8783db88a4b767228
Blaž <121607358+Blaz05@users.noreply.github.com>
2025-07-17 18:37:30 +0200

    Deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-from Esercitazione1 import MovingAverage
-from unittest import TestCase
-
-class Test(TestCase):
-    def setUp(self):
-        self.MovingAverage = MovingAverage(2)
-    
-    def test_media(self):
-        self.assertEqual(self.MovingAverage.compute([2,4]), 3.0)
