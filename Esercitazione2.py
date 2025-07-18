f528079d0479194b1274e806c8f873305c95c09b
Blaž <121607358+Blaz05@users.noreply.github.com>
2025-07-18 18:53:37 +0200

    Completata

--- Esercitazione2.py
+++ Esercitazione2.py
@@ -2,63 +2,68 @@ class ExamException(Exception):
             pass
 
 class CSVTimeSeriesFile:
-    def __init__(self, nome):
-        self.name = nome
+    def __init__(self, name):
+        self.name = name
     
     def get_data(self):
-        contenuto = []
         try:
             with open(self.name, 'r') as testo:
-                for linea in testo:
-                    elementi = linea.split(',')
-                    if elementi[0] != 'date':
-                        try:
-                            elementi[1] = int(elementi[1])
-                        except:
-                            elementi[1] = 0
-                    contenuto.append(elementi)
-        except Exception as e:
-            print(f"Impossibile aprire il file. Errore: {e}")
-        return contenuto[1:]
-    
+                try:
+                    contenuto = []
+                    for linea in testo:
+                        lista = linea.strip().split(',')
+                        contenuto.append(lista)
+                except:
+                    raise ExamException('Il file non è leggibile')
+        except:
+            raise FileExistsError('Il file non esiste.')
+        for lista in contenuto:
+            if lista[0] != 'date':
+                try:
+                    lista[1] = int(lista[1])
+                except:
+                    lista[1] = 'Mancante'          
+        return contenuto
 
-time_series_file = CSVTimeSeriesFile('data.csv')
+time_series_file = CSVTimeSeriesFile(name='data.csv') 
 time_series = time_series_file.get_data()
 
-
 def compute_variations(time_series, first_year, last_year):
+    
     try:
         first_year = int(first_year)
         last_year = int(last_year)
-    except Exception as error:
-        print(f"Inserire valore valido. Errore: {error}")
-
-    diz_passeggeri = {}
+    except:
+        raise ExamException('Impossibile convertire in intero.')
+    
+    if first_year > last_year:
+        raise ValueError("L'anno di partenza non può essere più grande dell'anno finale")
+    
+    passeggeri_per_anno = {}
     for lista in time_series:
-        data = lista[0].split('-')
-        anno = int(data[0])
-        if anno >=first_year and anno <= last_year:
-            if anno not in diz_passeggeri.keys():
-                diz_passeggeri[anno] = []
-            diz_passeggeri[anno].append(lista[1])
+        if lista[0] != 'date':
+            data = lista[0].strip().split('-')
+            anno = int(data[0])
+            if anno not in passeggeri_per_anno.keys():
+                passeggeri_per_anno[anno] = []
+            if isinstance(lista[1], int):
+                passeggeri_per_anno[anno].append(lista[1])
+    
+    media_passeggeri_per_anno = {}
+    for anno in passeggeri_per_anno.keys():
+        media_passeggeri_per_anno[anno] = sum(passeggeri_per_anno[anno])/len(passeggeri_per_anno[anno])
 
-    medie_annuali = []
-    for anno in sorted(diz_passeggeri.keys()):
-        somma = sum(diz_passeggeri[anno])
-        lunghezza =  len(diz_passeggeri[anno])
-        media = somma/lunghezza
+    differenze = []
+    anno_prev = None
+    for anno in media_passeggeri_per_anno.keys():
+        if anno_prev != None and anno >= first_year and anno <= last_year:
+            diff = media_passeggeri_per_anno[anno]-media_passeggeri_per_anno[anno_prev]
+            differenze.append([f'{anno_prev}-{anno}', diff])
+        anno_prev = anno
+    
+    return differenze
 
-    dizionario_variazioni = {}
-    for i in range(len(medie_annuali) - 1):
-        # medie_annuali[i] = [anno_i, media_i]
-        # medie_annuali[i+1] = [anno_{i+1}, media_{i+1}]
+print(compute_variations(time_series,1949,1955))
 
-        # Chiave = "anno_i-anno_{i+1}" 
-        chiave = str(medie_annuali[i][0]) + '-' + str(medie_annuali[i+1][0])
-        # Valore = media_{i+1} - media_i
-        valore = medie_annuali[i+1][1]-medie_annuali[i][1]
-        dizionario_variazioni[chiave] = valore
-    return dizionario_variazioni
 
-print(compute_variations(time_series,1950,1960))
 
