7c6812cd3f48ebd1da1af7a796f289a01e1729f5
Blaž <121607358+Blaz05@users.noreply.github.com>
2025-07-19 09:39:37 +0200

    Con Chat

--- Esercitazione1.py
+++ Esercitazione1.py
@@ -1,27 +1,28 @@
-class MovingAverage:
-    def __init__(self, window):
-        self.window = window
-    
-    def compute(self, lista):
-        if not type(lista) == list:
-            raise ExamException("L'input fornito non è una lista.")
-        if not len(lista) >= 2:
-            raise ExamException("Fornire una lista con almeno due elementi, per calcolare la media mobile")
-        mobile = []
-        prev_element = None
-        try:
-            for element in lista:
-                element = float(element)
-                if(prev_element != None):
-                    mobile.append((prev_element+element)/2)
-                prev_element = element
-        except:
-            raise ExamException("Fornire elementi validi")
-        return mobile
-
 class ExamException(Exception):
     pass
 
-moving_average = MovingAverage(2)
-result = moving_average.compute([2,4,8,16]) 
-print(result)
+class MovingAverage:
+    def __init__(self, N):
+        self.N = N
+        try:
+            self.N = int(N)
+        except:
+            raise ExamException('Inserire un numero intero.')
+    
+    def compute(self, lista):
+        media_mobile = []
+        try:
+            float_list = []
+            for el in lista:
+                float_list.append(float(el))
+            for i in range(len(float_list) - self.N + 1):
+                N_values = float_list[i:i + self.N]
+                average = sum(N_values) / self.N
+                media_mobile.append(average)
+        except:
+            raise ExamException("Fornire elementi numerici validi.")
+        return media_mobile
+
+moving_average = MovingAverage(3)
+result = moving_average.compute([2,4,8,16,32,64,128,256,512]) 
+print(result) # Deve stampare a schermo [3.0,6.0,12.0]
